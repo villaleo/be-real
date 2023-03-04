5f63e7c92a7817c38371ffd11d2173b30bd374cc
Leo Villalobos <villaleobos@gmail.com>
2023-03-03 20:38:22 -0800

    Add Post model

--- lab-insta-parse/Models/Post.swift
+++ lab-insta-parse/Models/Post.swift
@@ -6,13 +6,35 @@
 //
 
 import Foundation
+import ParseSwift
 
-// TODO: Pt 1 - Import Parse Swift
-
-
-// TODO: Pt 1 - Create Post Parse Object model
-// https://github.com/parse-community/Parse-Swift/blob/3d4bb13acd7496a49b259e541928ad493219d363/ParseSwift.playground/Pages/1%20-%20Your%20first%20Object.xcplaygroundpage/Contents.swift#L33
-
-struct Post {
+struct Post: ParseObject {
+    var caption: String?
+    var user: User?
+    var imageFile: ParseFile?
     
+    var originalData: Data?
+    var objectId: String?
+    var createdAt: Date?
+    var updatedAt: Date?
+    var ACL: ParseSwift.ParseACL?
+}
+
+// impl Post
+extension Post {
+    init(caption: String, user: User, imageFile: ParseFile) {
+        self.caption = caption
+        self.user = user
+        self.imageFile = imageFile
+    }
+}
+
+// MARK: Confirm Post to CustomStringConvertible
+extension Post: CustomStringConvertible {
+    var description: String {
+        guard let user = user else {
+            return "Post{caption=\(caption ?? ""),user=<nil>"
+        }
+        return "Post{caption=\(caption ?? ""),user=\(user)"
+    }
 }
